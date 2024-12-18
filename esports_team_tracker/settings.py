41dd8a7398767eb872831325bfb09ed374e5a678
SscottK <gr8whtninjassk@gmail.com>
2024-12-17 22:07:41 -0800

    First migration, added PSQL db info

--- esports_team_tracker/settings.py
+++ esports_team_tracker/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'estt_main_app'
+    'estt_main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'esports_team_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'esttdb',
     }
 }
 
